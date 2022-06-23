7fe1fd459a7b428498cfa116adeb369370cc6c44
Waqar Ali <waqar.ali@prixite.com>
2022-06-23 23:57:25 +0500

    Setting up CI

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,9 @@
-from rest_framework import  serializers
+from rest_framework import serializers
+
 from core import models
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ['first_name','last_name']
+        fields = ["first_name", "last_name"]
