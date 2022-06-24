a2b8fa66b8653a2557fd6687301bf0c76259c062
Waqar Ali <waqar.ali@prixite.com>
2022-06-24 09:34:19 +0500

    Format

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,55 +1,51 @@
 import React from "react";
 import { ReactDOM } from "react/client";
-import './styles/style.css'
+import "./styles/style.css";
 
 class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
 }
 
 class Board extends React.Component {
-    renderSquare(number) {
-        <Square id={number} />
-    }
-    render() {
-        const status = 'X'
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+  renderSquare(number) {
+    <Square id={number} />;
+  }
+  render() {
+    const status = "X";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
 
 class GameBoard extends React.Component {
-    render() {
-        return (
-            <div>
-                <Board />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Board />
+      </div>
+    );
+  }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('contianer'))
-root.render(<GameBoard />)
+const root = ReactDOM.createRoot(document.getElementById("contianer"));
+root.render(<GameBoard />);
