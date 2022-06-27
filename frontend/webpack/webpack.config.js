dea5453dbb60ef6bd76e6c8eb644d5c696c15bee
Waqar Ali <waqar.ali@prixite.com>
2022-06-27 19:19:31 +0500

    Setup react app: IP

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -2,34 +2,11 @@
 
 const path = require("path");
 
-const webpack = require("webpack");
-
 module.exports = {
+  mode: "development",
   entry: "/frontend/src/index.js",
   output: {
-    path: path.resolve(__dirname, "./dist/"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist/"),
+    filename: "main.js",
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("development"),
-      },
-    }),
-  ],
 };
