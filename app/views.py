d364ee06789c534494abecdbd71fdde4d0adf40c
Waqar Ali <waqar.ali@prixite.com>
2022-07-14 11:20:30 +0500

    Formatting

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
-from django.views.generic import TemplateView
-from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from rest_framework.viewsets import ModelViewSet
+
 from app import models, serializers
 
 # Create your views here.
 
-@method_decorator(login_required,name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
 class HomeView(TemplateView):
     template_name = "core/home.html"
 
