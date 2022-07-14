5071e65975ccc508eddf5f258b296ed68bbfa906
Umair Khan <umair.khan@toptal.com>
2022-07-14 13:09:37 +0500

    Use url tag in templates

--- app/urls/html.py
+++ app/urls/html.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from app.views import html as views
 
+app_name = "html"
+
 urlpatterns = [
-    path("", views.Home.as_view()),
+    path("", views.Home.as_view(), name='home'),
+    path("users/", views.Users.as_view(), name='users'),
+    path("employees/", views.Employees.as_view(), name='employees'),
+    path("settings/", views.Settings.as_view(), name='settings'),
+    path("account/", views.Account.as_view(), name='account'),
 ]
--- app/views/html.py
+++ app/views/html.py
@@ -3,3 +3,19 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "app/html/home.html"
+
+
+class Users(TemplateView):
+    template_name = "app/html/users.html"
+
+
+class Employees(TemplateView):
+    template_name = "app/html/employees.html"
+
+
+class Settings(TemplateView):
+    template_name = "app/html/settings.html"
+
+
+class Account(TemplateView):
+    template_name = "app/html/account.html"
