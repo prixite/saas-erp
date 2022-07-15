f485372c9ab9999b391cf190a4038624b16b8db1
Muhammad Shafiq <muhammad.shafiq@evampsaanga.com>
2022-07-15 13:56:17 +0500

    Pre-commit fixes

--- app/urls/html.py
+++ app/urls/html.py
@@ -5,9 +5,9 @@ from app.views import html as views
 app_name = "html"
 
 urlpatterns = [
-    path("", views.Home.as_view(), name='home'),
-    path("users/", views.Users.as_view(), name='users'),
-    path("employees/", views.Employees.as_view(), name='employees'),
-    path("settings/", views.Settings.as_view(), name='settings'),
-    path("account/", views.Account.as_view(), name='account'),
+    path("", views.Home.as_view(), name="home"),
+    path("users/", views.Users.as_view(), name="users"),
+    path("employees/", views.Employees.as_view(), name="employees"),
+    path("settings/", views.Settings.as_view(), name="settings"),
+    path("account/", views.Account.as_view(), name="account"),
 ]
