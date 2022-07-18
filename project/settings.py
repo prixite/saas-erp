fb02ef2763614f027fea645628dd3cb90770012f
Waqar Ali <waqar.ali@prixite.com>
2022-07-18 10:23:56 +0500

    Password Reset setup

--- project/settings.py
+++ project/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = "static/"
 STATICFILES_DIRS = [BASE_DIR / "frontend/dist/"]
-
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
