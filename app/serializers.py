c880ab8fc2e2512e344f2e30b921c17ba036bd60
Waqar Ali <waqar.ali@prixite.com>
2022-07-19 18:07:53 +0500

    Remove dummy serializer

--- app/serializers.py
+++ app/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from app import models
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.User
-        fields = ["first_name", "last_name"]
-
-
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Employee
