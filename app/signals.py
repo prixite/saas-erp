882d2097b9f74cfa97bb56ec9893f34b146a924b
Waqar Ali <waqar.ali@prixite.com>
2022-08-04 18:15:45 +0500

    Format new file

--- app/signals.py
+++ app/signals.py
@@ -1,11 +1,13 @@
 from django.db.models.signals import post_save
-
 from django.dispatch import receiver
 
 from app import models
 
-@receiver(post_save,sender=models.Employee)
-def create_user(sender,**kwargs):
-    if kwargs['created']:
-        models.User.objects.create(username=None,email=None,is_active=kwargs['created']['user_allowed'])
+
+@receiver(post_save, sender=models.Employee)
+def create_user(sender, **kwargs):
+    if kwargs["created"]:
+        models.User.objects.create(
+            username=None, email=None, is_active=kwargs["created"]["user_allowed"]
+        )
         # Create a user here, allow/disallow user login.
