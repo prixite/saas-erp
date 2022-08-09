917c040f47f61e3788531f04c639cc876cc6f209
Waqar Ali <waqar.ali@prixite.com>
2022-08-09 11:33:11 +0500

    Improvements

--- app/signals.py
+++ app/signals.py
@@ -2,12 +2,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from app import models
-
+from app.lib.user import create_and_send_invite
 
 @receiver(post_save, sender=models.Employee)
 def create_user(sender, **kwargs):
-    if kwargs["created"]:
-        models.User.objects.create(
-            username=None, email=None, is_active=kwargs["created"]["user_allowed"]
+    if kwargs["created"] and kwargs['instance'].user_allowed:
+        user = models.User.objects.create(
+            is_active=kwargs["instance"].user_allowed # pass in email from employee creation into this.
         )
-        # Create a user here, allow/disallow user login.
+        create_and_send_invite(user)
