7d62ce9db3b73567bf79f8d5bcab7c3dc2da8ffe
Waqar Ali <waqar.ali@prixite.com>
2022-08-11 09:49:33 +0500

    Create user with employee

--- app/models/employee.py
+++ app/models/employee.py
@@ -27,9 +27,6 @@ class Employee(models.Model):
     class Meta:
         ordering = ["-id"]
 
-    def save(self,user_args,**kwargs):
-        self.user = AUTH_USER_MODEL.objects.create(username=user_args['username'],email=user_args['email'])
-        return super().save(**kwargs)
 
 class Document(models.Model):
     """
--- app/serializers.py
+++ app/serializers.py
@@ -8,19 +8,26 @@ class EmployeeListSerializer(serializers.ModelSerializer):
         model = models.Employee
         fields = [
             "id",
-            "name",
             "contact_number",
             "date_of_joining",
         ]
 
 
+class EmployeeUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = ["first_name", "email"]
+
+
 class EmployeeSerializer(serializers.ModelSerializer):
+    user = EmployeeUserSerializer(write_only=True)
+
     class Meta:
         model = models.Employee
 
         fields = [
             "id",
-            "name",
+            "user",
             "contact_number",
             "date_of_joining",
             "nic",
@@ -33,6 +40,14 @@ class EmployeeSerializer(serializers.ModelSerializer):
         ]
 
         read_only_fields = ["degrees"]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user = models.User.objects.create(
+            username=user_data["email"],
+            first_name=user_data["first_name"],
+        )
+        return models.Employee.objects.create(user=user, **validated_data)
 
 
 class CompensationSerializer(serializers.ModelSerializer):
