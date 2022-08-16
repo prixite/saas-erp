0eb5ee69462352b4fde77e6764045607dbcc01ca
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-08-16 17:00:52 +0500

    Added context processor

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,7 +1,17 @@
 def accessible_modules(request):
-    print("::::::::::::::::::::::::::::::member_modules::::::::::::::::",request.user.member_modules)
-    print("::::::::::::::::::::::::::::::::::admin_modules::::::::::::",request.user.admin_modules)
-    print("::::::::::::::::::::::::::::::::::owner_modules::::::::::::",request.user.owner_modules)
-    return {
-        "MY_CUSTOM_NAME":"Aftab"
-    }
+
+    is_allowed = False
+
+    if not request.user.is_authenticated:
+        is_allowed = True
+
+        if request.user.is_superuser:
+            is_allowed = True
+
+        if self.module in [x.slug for x in request.user.member_modules]:
+            if self.module in [x.slug for x in request.user.admin_modules]:
+                is_allowed = True
+            if self.module in [x.slug for x in request.user.owner_modules]:
+                is_allowed = True
+
+    return {"is_allowed": is_allowed}
