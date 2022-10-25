dd026ae2a26802334b04956c13177703144b38f2
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-10-25 16:46:56 +0500

    Border hover

--- frontend/src/components/common/smart/dashboard/headbar/searchAreaBox/SearchAreaBox.tsx
+++ frontend/src/components/common/smart/dashboard/headbar/searchAreaBox/SearchAreaBox.tsx
@@ -8,6 +8,25 @@ function SearchAreaBox() {
       id="search-headbox"
       variant="outlined"
       placeholder="Search Employee here"
+      sx={{
+        "& label.Mui-focused": {
+          color: "#999999",
+        },
+        "& .MuiInput-underline:after": {
+          borderBottomColor: "#E7E7E7",
+        },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: "#E7E7E7",
+          },
+          "&:hover fieldset": {
+            borderColor: "#999999",
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: "#999999",
+          },
+        },
+      }}
       InputProps={{
         startAdornment: (
           <InputAdornment position="start">
