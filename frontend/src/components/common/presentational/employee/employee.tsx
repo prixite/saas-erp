f2cfd4c924e64002efe5febadb0ca4c91da87bab
Faisal Usman <faisal.usman@prixite.com>
2022-10-27 14:03:16 +0500

    feedback changes.

--- frontend/src/components/common/presentational/employee/employee.tsx
+++ frontend/src/components/common/presentational/employee/employee.tsx
@@ -2,13 +2,24 @@
 // import EmployeeDetails from "@src/components/employee/employee-details";
 import { Box } from "@mui/system";
 import EmployeeListing from "@src/components/common/presentational/employee/employee-listing";
+import HeadBar from "@src/components/common/smart/dashboard/headbar/HeadBar";
+import { useGetEmployeesQuery } from "@src/store/reducers/employees-api";
 
 const Employee = () => {
+  const { data, isSuccess } = useGetEmployeesQuery();
   return (
     <Box>
-      {/* <EmployeeBio />
+      {isSuccess ? (
+        <>
+          <HeadBar />
+          <div>{data.map((item) => item.name)}</div>
+          {/* <EmployeeBio />
       <EmployeeDetails /> */}
-      <EmployeeListing />
+          <EmployeeListing />
+        </>
+      ) : (
+        ""
+      )}
     </Box>
   );
 };
--- frontend/src/components/common/smart/dashboard/dashboard.tsx
+++ frontend/src/components/common/smart/dashboard/dashboard.tsx
@@ -1,26 +1,13 @@
 import { Typography, Box } from "@mui/material";
-import HeadBar from "@src/components/common/smart/dashboard/headbar/HeadBar";
 import DashboardSkeleton from "@src/components/shared/loaders/dashboardSkeleton/dashboardSkeleton";
 import RowSkeletonCard from "@src/components/shared/loaders/rowSkeletonCard/rowSkeletonCard";
 
-import { useGetEmployeesQuery } from "@src/store/reducers/employees-api";
-
 const Dashboard = () => {
-  const { data, isSuccess } = useGetEmployeesQuery();
   return (
     <Box>
       <Typography variant="h1">Dashboard</Typography>
-      {isSuccess ? (
-        <>
-          <HeadBar />
-          <div>{data.map((item) => item.name)}</div>
-        </>
-      ) : (
-        <>
-          <DashboardSkeleton />
-          <RowSkeletonCard />
-        </>
-      )}
+      <DashboardSkeleton />
+      <RowSkeletonCard />
       {/* To Test Display Skeleton! */}
       {/* <DashboardSkeleton /> */}
     </Box>
