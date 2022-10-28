7956547dcf2a84b2bc2b9f385eaa355366fa0332
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-10-28 17:59:29 +0500

    Conflict resolve

--- frontend/src/components/common/smart/dashboard/dashboard.tsx
+++ frontend/src/components/common/smart/dashboard/dashboard.tsx
@@ -1,11 +1,15 @@
 import { Typography, Box } from "@mui/material";
-import DataGridTable from "@src/components/shared/dataGrid/DataGridTable";
+import DashboardSkeleton from "@src/components/shared/loaders/dashboardSkeleton/dashboardSkeleton";
+import RowSkeletonCard from "@src/components/shared/loaders/rowSkeletonCard/rowSkeletonCard";
 
 const Dashboard = () => {
   return (
     <Box>
       <Typography variant="h1">Dashboard</Typography>
-      <DataGridTable />
+      <DashboardSkeleton />
+      <RowSkeletonCard />
+      {/* To Test Display Skeleton! */}
+      {/* <DashboardSkeleton /> */}
     </Box>
   );
 };
