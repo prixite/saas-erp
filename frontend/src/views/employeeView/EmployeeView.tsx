547e455302db680bcbcb3abc179876d0f8fe069e
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-11-01 16:51:24 +0500

    FolderStructure and EmployeeSection Route

---
+++ frontend/src/views/employeeView/EmployeeView.tsx
@@ -0,0 +1,6 @@
+import EmployeeSection from "@src/components/common/smart/employeeSection/EmployeeSection";
+function EmployeeView() {
+  return <EmployeeSection />;
+}
+
+export default EmployeeView;
