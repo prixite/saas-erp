6e6ce8d8495dc0bd9b81830c24cb6fea4f6130bb
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-01 11:26:07 +0500

    formating

--- app/migrations/0027_user_headline_alter_user_image.py
+++ app/migrations/0027_user_headline_alter_user_image.py
@@ -6,19 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0026_remove_employee_image_user_image'),
+        ("app", "0026_remove_employee_image_user_image"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='headline',
-            field=models.CharField(default='', max_length=255, verbose_name='headline'),
+            model_name="user",
+            name="headline",
+            field=models.CharField(default="", max_length=255, verbose_name="headline"),
             preserve_default=False,
         ),
         migrations.AlterField(
-            model_name='user',
-            name='image',
-            field=models.ImageField(default='../static/app/assets/profile_default.png', null=True, upload_to='profile'),
+            model_name="user",
+            name="image",
+            field=models.ImageField(
+                default="../static/app/assets/profile_default.png",
+                null=True,
+                upload_to="profile",
+            ),
         ),
     ]
