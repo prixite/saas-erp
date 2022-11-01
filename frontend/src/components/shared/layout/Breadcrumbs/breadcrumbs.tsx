fafff508aec8c08ec20d1ab83250b37429c1a274
Faisal Usman <faisal.usman@prixite.com>
2022-11-02 00:08:26 +0500

    feedback changes

--- frontend/src/components/shared/layout/Breadcrumbs/breadcrumbs.tsx
+++ frontend/src/components/shared/layout/Breadcrumbs/breadcrumbs.tsx
@@ -1,8 +1,10 @@
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { useLocation, useNavigate } from "react-router-dom";
 import { capitalizeFirstLowercaseRest } from "@src/helpers/utils/utils";
+import "@src/components/shared/layout/Breadcrumbs/breadcrumbs.scss";
 
 const BreadCrumbs = () => {
   const location = useLocation();
@@ -10,38 +12,51 @@ const BreadCrumbs = () => {
   const pathnames = location.pathname.split("/").filter((x) => x);
 
   return (
-    <>
-      <Breadcrumbs
-        separator="›"
-        aria-label="breadcrumb"
-        sx={{ ml: "25px", flexGrow: 1, display: { xs: "none", sm: "block" } }}
-      >
-        {pathnames.map((name, index) => {
-          const routeTo = `/${pathnames.slice(0, index + 1).join("/")}/`;
-          const isLast = index === pathnames.length - 1;
-          return isLast ? (
-            <Typography
-              key={name}
-              fontWeight={400}
-              variant={"h5"}
-              color="text.primary"
-            >
-              {capitalizeFirstLowercaseRest(name)}
-            </Typography>
-          ) : (
-            <Link
-              underline="hover"
-              key={name}
-              color="inherit"
-              onClick={() => navigate(routeTo)}
-              sx={{ fontSize: "14px", fontWeight: "400" }}
-            >
-              {capitalizeFirstLowercaseRest(name)}
-            </Link>
-          );
-        })}
-      </Breadcrumbs>
-    </>
+    <Breadcrumbs
+      separator={<NavigateNextIcon fontSize="medium" />}
+      aria-label="breadcrumb"
+      sx={{ ml: "25px", flexGrow: 1, display: { xs: "none", sm: "block" } }}
+    >
+      {pathnames.length > 0 ? (
+        <Link
+          sx={{ fontSize: "14px", fontWeight: "400", cursor: "pointer" }}
+          underline="hover"
+          color="inherit"
+          onClick={() => navigate("/")}
+        >
+          Home
+        </Link>
+      ) : (
+        <Typography fontWeight={400} variant={"h5"} color="inherit">
+          {" "}
+          Home{" "}
+        </Typography>
+      )}
+      {pathnames.map((name, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}/`;
+        const isLast = index === pathnames.length - 1;
+        return isLast ? (
+          <Typography
+            key={name}
+            fontWeight={400}
+            variant={"h5"}
+            color="text.primary"
+          >
+            {capitalizeFirstLowercaseRest(name)}
+          </Typography>
+        ) : (
+          <Link
+            underline="hover"
+            key={name}
+            color="inherit"
+            onClick={() => navigate(routeTo)}
+            sx={{ fontSize: "14px", fontWeight: "400", cursor: "pointer" }}
+          >
+            {capitalizeFirstLowercaseRest(name)}
+          </Link>
+        );
+      })}
+    </Breadcrumbs>
   );
 };
 export default BreadCrumbs;
