565ffb66d2472bf373ac927e3841999d160813fd
Rabeel7 <107916405+Rabeel7@users.noreply.github.com>
2022-11-02 17:27:40 +0500

    Routing 195 (#240)

--- frontend/src/components/common/presentational/employee/employee.tsx
+++ frontend/src/components/common/presentational/employee/employee.tsx
@@ -1,21 +1,23 @@
 // import EmployeeBio from "@src/components/employee/employee-bio";
 // import EmployeeDetails from "@src/components/employee/employee-details";
 import { Box } from "@mui/system";
-import EmployeeListing from "@src/components/common/presentational/employee/employee-listing";
+import DataGridTable from "@src/components/common/presentational/dataGridTable/DataGridTable";
+// import EmployeeListing from "@src/components/common/presentational/employee/employee-listing";
 import HeadBar from "@src/components/common/smart/dashboard/headbar/HeadBar";
 import { useGetEmployeesQuery } from "@src/store/reducers/employees-api";
 
 const Employee = () => {
-  const { data, isSuccess } = useGetEmployeesQuery();
+  const { isSuccess } = useGetEmployeesQuery();
   return (
     <Box>
       {isSuccess ? (
         <>
           <HeadBar />
-          <div>{data.map((item) => item.name)}</div>
+          {/* <div>{data.map((item) => item.name)}</div> */}
           {/* <EmployeeBio />
           <EmployeeDetails /> */}
-          <EmployeeListing />
+          {/* <EmployeeListing /> */}
+          <DataGridTable />
         </>
       ) : (
         ""
