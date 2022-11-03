e54535353187ddfd70a702d98e3f75d2d825fd48
Faisal Usman <faisal.usman@prixite.com>
2022-11-03 11:12:31 +0500

    feedback changes

--- frontend/src/components/common/smart/payroll/payroll.tsx
+++ frontend/src/components/common/smart/payroll/payroll.tsx
@@ -1,9 +1,13 @@
 import { Typography, Box } from "@mui/material";
+import { LocalizationInterface } from "@src/helpers/interfaces/localizationinterfaces";
+import { localizedData } from "@src/helpers/utils/language";
 
 const payroll = () => {
+  const constantData: LocalizationInterface = localizedData();
+  const { payrollHeading } = constantData.Payroll;
   return (
     <Box>
-      <Typography variant="h1">Payroll</Typography>
+      <Typography variant="h1">{payrollHeading}</Typography>
     </Box>
   );
 };
--- frontend/src/helpers/interfaces/localizationinterfaces.ts
+++ frontend/src/helpers/interfaces/localizationinterfaces.ts
@@ -5,8 +5,24 @@ export interface btnsInterface {
 export interface employeeInterface {
   employeeHeading: string;
 }
+export interface userInterface {
+  userHeading: string;
+}
+export interface payrollInterface {
+  payrollHeading: string;
+}
+export interface accountInterface {
+  accountHeading: string;
+}
+export interface dashboardInterface {
+  dashboardHeading: string;
+}
 
 export interface LocalizationInterface {
   Buttons: btnsInterface;
   Employee: employeeInterface;
+  Dashboard: dashboardInterface;
+  User: userInterface;
+  Payroll: payrollInterface;
+  Account: accountInterface;
 }
