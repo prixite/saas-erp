5043ef3eeccdd65a454fad4bcd7bdd90bf18dc25
Rabeel Qaiser <rabeel.qaiser@prixite.com>
2022-11-07 19:07:45 +0500

    Feedback Updated

--- frontend/src/components/common/presentational/employeeButtons/EmployeeButtons.tsx
+++ frontend/src/components/common/presentational/employeeButtons/EmployeeButtons.tsx
@@ -1,58 +1,98 @@
-import { Button, Grid } from "@mui/material";
+import { useMemo, useState } from "react";
+import { Grid } from "@mui/material";
 import "./employeeButtons.scss";
-
+import { styled } from "@mui/material/styles";
+import MuiToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 interface EmployeeButtonType {
   setButtonNameClicked: React.Dispatch<React.SetStateAction<string>>;
 }
+
+interface toggleButtonType {
+  selectedColor: string;
+}
+const ToggleButton = styled(MuiToggleButton)(
+  ({ selectedColor }: toggleButtonType) => ({
+    "&.Mui-selected, &.Mui-selected:hover": {
+      color: "white",
+      backgroundColor: selectedColor,
+    },
+  })
+);
+
 function EmployeeButtons({ setButtonNameClicked }: EmployeeButtonType) {
-  const BasicInformationHandler = () => {
-    setButtonNameClicked("BASIC");
+  const [alignment, setAlignment] = useState<string | null>("left");
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment(newAlignment);
   };
-  const DocumentsHandler = () => {
-    setButtonNameClicked("DOCS");
-  };
-  const CompensationBenefitsHandler = () => {
-    setButtonNameClicked("COMP");
-  };
+
+  useMemo(() => {
+    if (alignment === "left") {
+      setButtonNameClicked("BASIC");
+    } else if (alignment === "center") {
+      setButtonNameClicked("DOCS");
+    } else if (alignment === "right") {
+      setButtonNameClicked("COMP");
+    } else {
+      setButtonNameClicked("BASIC");
+    }
+  }, [alignment]);
+
   return (
     <>
-      {/* // <div>EmployeeButtons</div> */}
       <Grid container className="employeeButton-main">
-        <Grid item className="button-container-one">
-          <Button
+        <ToggleButtonGroup
+          value={alignment}
+          exclusive
+          onChange={handleAlignment}
+          className="toggleGroup"
+        >
+          <ToggleButton
+            value="left"
+            selectedColor="red"
             className="buttonOne"
-            variant="contained"
-            color="secondary"
-            //   startIcon={<DeleteIcon />}
-            onClick={BasicInformationHandler}
+            style={{ borderRadius: "12px", marginRight: "12px" }}
           >
-            <p className="buttone-one-text">Basic Information </p>
-          </Button>
-        </Grid>
+            <span
+              className="button-one-text"
+              style={{ color: `${alignment === "left" ? "white" : "black"}` }}
+            >
+              Basic Information
+            </span>
+          </ToggleButton>
 
-        <Grid item className="button-container-two">
-          <Button
+          <ToggleButton
+            value="center"
+            selectedColor="red"
+            style={{ borderRadius: "12px", marginRight: "12px" }}
             className="buttonTwo"
-            variant="contained"
-            color="secondary"
-            //   startIcon={<DeleteIcon />}
-            onClick={DocumentsHandler}
           >
-            <p className="buttone-two-text">Documents </p>
-          </Button>
-        </Grid>
+            <span
+              className="button-two-text"
+              style={{ color: `${alignment === "center" ? "white" : "black"}` }}
+            >
+              Documents
+            </span>
+          </ToggleButton>
 
-        <Grid item className="button-container-three">
-          <Button
+          <ToggleButton
+            value="right"
+            selectedColor="red"
+            style={{ borderRadius: "12px" }}
             className="buttonThree"
-            variant="contained"
-            color="secondary"
-            //   startIcon={<DeleteIcon />}
-            onClick={CompensationBenefitsHandler}
           >
-            <p className="buttone-three-text">Compensation and benefits </p>
-          </Button>
-        </Grid>
+            <span
+              className="button-three-text"
+              style={{ color: `${alignment === "right" ? "white" : "black"}` }}
+            >
+              Compensation and benefits
+            </span>
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Grid>
     </>
   );
