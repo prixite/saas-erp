a8458ba8d262e3b6b3f61ed867ed13bea6c900f3
Umair Jameel <umair.jameel@prixite.com>
2022-11-11 12:51:33 +0500

    Change baseurl.

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   plugins: [
     new EnvironmentPlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
-      REACT_APP_GOOGLE_CLIENT_ID: "fake"
+      REACT_APP_GOOGLE_CLIENT_ID: "fake",
     }),
     new BundleTracker({ filename: "./webpack-stats.json" }),
   ],
