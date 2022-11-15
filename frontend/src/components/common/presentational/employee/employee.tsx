b87a6f58cd36f816be369ae61153225605a1cbc7
Faisal Usman <faisal.usman@prixite.com>
2022-11-15 15:12:34 +0500

    employee listing api

--- frontend/src/components/common/presentational/employee/employee.tsx
+++ frontend/src/components/common/presentational/employee/employee.tsx
@@ -1,8 +1,5 @@
-// import EmployeeBio from "@src/components/employee/employee-bio";
-// import EmployeeDetails from "@src/components/employee/employee-details";
 import { Box } from "@mui/system";
 import DataGridTable from "@src/components/common/presentational/dataGridTable/DataGridTable";
-// import EmployeeListing from "@src/components/common/presentational/employee/employee-listing";
 import HeadBar from "@src/components/common/smart/dashboard/headbar/HeadBar";
 
 const Employee = () => {
@@ -10,10 +7,6 @@ const Employee = () => {
     <Box>
       <>
         <HeadBar />
-        {/* <div>{data.map((item) => item.name)}</div> */}
-        {/* <EmployeeBio />
-          <EmployeeDetails /> */}
-        {/* <EmployeeListing /> */}
         <DataGridTable />
       </>
     </Box>
