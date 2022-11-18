8ca85e8bd9e16502af4a93646a1167ca04f74d32
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-18 17:53:39 +0500

    Test case update

--- app/tests/test_me.py
+++ app/tests/test_me.py
@@ -19,5 +19,6 @@ class MeTestCase(BaseTestCase):
                 "avatar",
                 "is_superuser",
                 "headline",
+                "contact_number"
             ],
         )
