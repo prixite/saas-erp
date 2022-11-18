e6b2c584139351e5435f957f38fe6435e58451d9
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-18 13:32:16 +0500

    Formating

--- app/views/api.py
+++ app/views/api.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from rest_framework.generics import RetrieveAPIView
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from app import models, serializers
 
