d1cca4f61dbbb54941eebc741eedccb317d54ef2
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-20 14:41:46 +0500

    temp

--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-unresolved */
 import { configureStore } from "@reduxjs/toolkit";
 import { employeesApi } from "@src/store/reducers/employees-api";
+import { emptySplitApi } from "@src/store/emptyApi";
 
 export const store = configureStore({
   reducer: {
     [employeesApi.reducerPath]: employeesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(employeesApi.middleware),
+    getDefaultMiddleware()
+      .concat(emptySplitApi.middleware)
+      .concat(employeesApi.middleware),
 });
--- frontend/src/views/employeeView/EmployeeView.tsx
+++ frontend/src/views/employeeView/EmployeeView.tsx
@@ -1,5 +1,12 @@
 import EmployeeSection from "@src/components/common/smart/employeeSection/EmployeeSection";
+import { useApiMeRetrieveQuery } from "@src/store/reducers/generated";
+import { useEffect } from "react";
 function EmployeeView() {
+  const data = useApiMeRetrieveQuery();
+  useEffect(() => {
+    console.log("data", data);
+  }, [data]);
+
   return <EmployeeSection />;
 }
 
