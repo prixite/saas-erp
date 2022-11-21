c43586f376e805ea35edded4d5dda5b9679ac0bf
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-21 13:45:28 +0500

    Formating

--- frontend/src/views/employeeView/EmployeeView.tsx
+++ frontend/src/views/employeeView/EmployeeView.tsx
@@ -1,12 +1,5 @@
-import { useEffect } from "react";
 import EmployeeSection from "@src/components/common/smart/employeeSection/EmployeeSection";
-import { useApiMeRetrieveQuery } from "@src/store/reducers/generated";
 function EmployeeView() {
-  const data = useApiMeRetrieveQuery();
-  useEffect(() => {
-    console.log("data", data);
-  }, [data]);
-
   return <EmployeeSection />;
 }
 
