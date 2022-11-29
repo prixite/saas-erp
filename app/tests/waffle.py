f1a1b20869a3d0cf8be6e82c1c88fa8f1c9fdbb5
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-11-29 11:39:31 +0500

    Feature flag api (#331)
    * Added feature flags through django waffle
    
    * Formating
    
    * Formating
    
    * Change to /api/flags
    
    * Map to flags
    
    Co-authored-by: Umair Khan <umair.waheed@gmail.com>

---
+++ app/tests/waffle.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+
+from app.tests.base import BaseTestCase
+
+
+class WaffleTestCase(BaseTestCase):
+    def test_me(self):
+        self.client.force_login(self.super_user)
+        response = self.client.get("/api/flags/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            list(response.json().keys()),
+            [
+                "name",
+                "active",
+            ],
+        )
