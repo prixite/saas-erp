cfc39495194853e51fd32aeddc149386c35f6167
Faisal Usman <faisal.usman@prixite.com>
2022-12-03 02:14:12 +0500

    employee basic information modal

--- frontend/src/components/common/smart/dashboard/headbar/createButton/CreateButton.tsx
+++ frontend/src/components/common/smart/dashboard/headbar/createButton/CreateButton.tsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
+import EmployeeModal from "@src/components/shared/popUps/employeeModal/employeeModal";
 import { LocalizationInterface } from "@src/helpers/interfaces/localizationinterfaces";
 import { localizedData } from "@src/helpers/utils/language";
 
 function CreateButton() {
   const constantData: LocalizationInterface = localizedData();
+  const [openModal, setOpenModal] = useState(false);
   const { createButton } = constantData.Buttons;
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
   return (
-    <>
+    <Box>
       <Button
         variant="outlined"
         className="create-btn"
         style={{ borderRadius: "12px" }}
         startIcon={<AddIcon />}
+        onClick={handleModalOpen}
       >
         {" "}
         <p id="create-btn-text">{createButton}</p>
       </Button>
-    </>
+      <EmployeeModal open={openModal} handleClose={handleModalClose} />
+    </Box>
   );
 }
 export default CreateButton;
---
+++ frontend/src/components/shared/popUps/employeeModal/pageThree.tsx
@@ -0,0 +1,5 @@
+const PageThree = () => {
+  return <div>pageThree</div>;
+};
+
+export default PageThree;
