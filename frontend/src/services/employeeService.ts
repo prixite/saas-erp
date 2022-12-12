546f383c5262317581e2ce5fcd5cd4f3a1cffbd1
Faisal Usman <faisal.usman@prixite.com>
2022-12-12 18:45:59 +0500

    create api

--- frontend/src/services/employeeService.ts
+++ frontend/src/services/employeeService.ts
@@ -1,13 +1,16 @@
 import { toast } from "react-toastify";
-
 import { timeOut } from "@src/helpers/constants/constants";
+import { EmployeeData } from "@src/helpers/interfaces/employees-modal";
 
 const deleteEmployeeService = async (rowCellId: number, deleteEmployee) => {
   await deleteEmployee({
     id: rowCellId,
   }).unwrap();
 };
-const addNewEmployeeService = async (employeeObject, createEmployee) => {
+const addNewEmployeeService = async (
+  employeeObject: EmployeeData[],
+  createEmployee
+) => {
   await createEmployee(employeeObject)
     .unwrap()
     .then(async () => {
