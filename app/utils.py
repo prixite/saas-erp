29dbd36b6c9722e7ef1bd7f449d8620e3fa3b3b0
Prixite <saad.ashraf@prixite.com>
2022-12-20 09:45:51 +0500

    change mail source.

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,12 @@
+from django.conf import settings
 from django.core.mail import send_mail
 
 
 def send_leave_email(to_email):
     subject = "Congrats! leave Granted"
     message = "leave grandted"
-    email_from = "into@example.com"
-    recipient_list = to_email
-
+    email_from = settings.DEFAULT_FROM_EMAIL
     try:
-        send_mail(subject, message, email_from, recipient_list)
+        send_mail(subject, message, email_from, to_email)
     except Exception as e:
         print(f"Exception when calling SMTPApi: {e}")
