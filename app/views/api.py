a658d2697bc1284194c753212ca24050734b03e6
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-12-20 14:13:12 +0500

    Formating

--- app/views/api.py
+++ app/views/api.py
@@ -11,7 +11,7 @@ from rest_framework import generics, status
 from rest_framework.authtoken import views as auth_views
 from rest_framework.authtoken.models import Token
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
