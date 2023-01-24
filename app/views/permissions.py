40a480702da43d7ff5e95fa1b151dc606fcaad95
AizaTariq97 <Aiza.tariq@prixite.com>
2023-01-24 16:04:48 +0500

    unnecessary changes delete

--- app/views/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-from rest_framework.permissions import BasePermission
-
-class IsNotSuperUserPermission(permissions.BasePermission):
-
-     def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        return super().has_permission(request, view)
