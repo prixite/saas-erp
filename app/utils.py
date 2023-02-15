39c7406e4dd944ef25bd7d9bff8e25712cd8f2e4
Prixite <saad.ashraf@prixite.com>
2023-02-15 13:17:13 +0500

    fixed changes.

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,3 @@
-import logging
-
 import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
@@ -70,18 +68,17 @@ def send_email_forget_password(data):
         return e
 
 
-def create_presigned_url(bucket_name, object_name, expiration=3600):
+def create_presigned_url(bucket_name, key, expiration=3600):
     # Generate a presigned URL for the S3 object
     s3_client = boto3.client("s3")
     try:
         response = s3_client.generate_presigned_url(
             "get_object",
-            Params={"Bucket": bucket_name, "Key": object_name},
+            Params={"Bucket": bucket_name, "Key": key},
             ExpiresIn=expiration,
         )
-    except ClientError as e:
-        logging.error(e)
-        return None
+    except ClientError:
+        raise
 
     # The response contains the presigned URL
     return response
