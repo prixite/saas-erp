f77bcc9ed422b746f000c728cdd5e0f1502a9c86
AizaTariq97 <116619133+AizaTariq97@users.noreply.github.com>
2023-02-21 13:35:59 +0500

    Update validations in remaining serializers (#542)

--- app/tests/test_employee.py
+++ app/tests/test_employee.py
@@ -518,7 +518,7 @@ class CurrencyTestCase(BaseTestCase):
         response = self.client.put(
             f"/api/currency/{self.currency.id}/", data=currency_data
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_currency_delete(self):
         self.client.force_login(self.owner)
