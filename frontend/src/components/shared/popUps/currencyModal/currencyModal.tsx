218812ad06661958441bb5100cc3b42686841947
Aftab Ahmad <aftab76ahmad76@gmail.com>
2023-02-23 14:14:22 +0500

    Formating

--- frontend/src/components/shared/popUps/currencyModal/currencyModal.tsx
+++ frontend/src/components/shared/popUps/currencyModal/currencyModal.tsx
@@ -16,11 +16,7 @@ import * as yup from "yup";
 import crossIcon from "@src/assets/svgs/cross.svg";
 import submitIcon from "@src/assets/svgs/Frame.svg";
 import { timeOut } from "@src/helpers/constants/constants";
-import {
-  CurrencyInterface,
-  LocalizationInterface,
-} from "@src/helpers/interfaces/localizationinterfaces";
-import { localizedData } from "@src/helpers/utils/language";
+import { CurrencyInterface } from "@src/helpers/interfaces/localizationinterfaces";
 import { toastAPIError } from "@src/helpers/utils/utils";
 import {
   useApiCurrencyCreateMutation,
