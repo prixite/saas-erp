33019d51cfe7cb69dda9a3282a6de22df8a902fc
Prixite <saad.ashraf@prixite.com>
2023-03-14 16:56:32 +0500

    fixes.

--- app/management/commands/attandence.py
+++ app/management/commands/attandence.py
@@ -1,10 +1,13 @@
-from django.core.management.base import BaseCommand
-from django.db import transaction
-from app import models
-from django.db import models as dj_models
-from app.utils import email_weekly_attandence
-from django.utils.timezone import now
 from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.db import models as dj_models
+from django.db import transaction
+from django.utils.timezone import now
+
+from app import models
+from app.utils import email_weekly_attandence
+
 
 class Command(BaseCommand):
     @transaction.atomic
@@ -12,20 +15,35 @@ class Command(BaseCommand):
         employee = models.Employee.objects.all()
         last_monday = now().date() - timedelta(days=now().weekday(), weeks=1)
         last_friday = last_monday + timedelta(days=4)
-        # ,created_at__date__range=[last_monday, last_friday]
         for i in employee:
-            attendance = models.Attendance.objects.filter(employee=i).values(
-                'employee', 
-            ).annotate(
-                time_in=dj_models.F('time_in'), 
-                time_out=dj_models.F('time_out'),
-                total_time=dj_models.ExpressionWrapper(
-                    dj_models.F('time_out') - dj_models.F('time_in'),
-                    output_field=dj_models.fields.DurationField()
+            attendance = (
+                models.Attendance.objects.filter(
+                    employee=i, created_at__date__range=[last_monday, last_friday]
+                )
+                .values(
+                    "employee",
+                )
+                .annotate(
+                    time_in=dj_models.F("time_in"),
+                    time_out=dj_models.F("time_out"),
+                    total_time=dj_models.ExpressionWrapper(
+                        dj_models.F("time_out") - dj_models.F("time_in"),
+                        output_field=dj_models.fields.DurationField(),
+                    ),
                 )
             )
-            total_working_hours = attendance.aggregate(total_time=dj_models.Sum(dj_models.F('time_out') - dj_models.F('time_in')))['total_time']
-            current_month_Leave = models.Leave.objects.filter(employee=i,created_at__month=now().month).count()
-            email_weekly_attandence(total_working_hours,current_month_Leave,attendance,i)
+            total_working_hours = attendance.aggregate(
+                total_time=dj_models.Sum(
+                    dj_models.F("time_out") - dj_models.F("time_in")
+                )
+            )["total_time"]
+            current_month_Leave = models.Leave.objects.filter(
+                employee=i, created_at__month=now().month
+            ).count()
+            email_weekly_attandence(
+                total_working_hours, current_month_Leave, attendance, i
+            )
 
-        self.stdout.write(self.style.SUCCESS("Weekly Attandence of employee email Successfully."))
+        self.stdout.write(
+            self.style.SUCCESS("Weekly Attandence of employee email Successfully.")
+        )
