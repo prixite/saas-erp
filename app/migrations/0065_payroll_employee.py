bc7e42e050cb0c55f24c84d8b5330e69644f4d73
Prixite <saad.ashraf@prixite.com>
2023-04-04 17:51:08 +0500

    fixes.

--- app/migrations/0065_payroll_employee.py
+++ app/migrations/0065_payroll_employee.py
@@ -7,14 +7,18 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0064_merge_20230301_0954'),
+        ("app", "0064_merge_20230301_0954"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='payroll',
-            name='employee',
-            field=models.OneToOneField(default=1, on_delete=django.db.models.deletion.CASCADE, to='app.employee'),
+            model_name="payroll",
+            name="employee",
+            field=models.OneToOneField(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="app.employee",
+            ),
             preserve_default=False,
         ),
     ]
--- app/migrations/0066_alter_payroll_employee.py
+++ app/migrations/0066_alter_payroll_employee.py
@@ -7,13 +7,15 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0065_payroll_employee'),
+        ("app", "0065_payroll_employee"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='payroll',
-            name='employee',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app.employee'),
+            model_name="payroll",
+            name="employee",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to="app.employee"
+            ),
         ),
     ]
